8a101b765f2fa45e1f50d5fbd743dde729972bd7
Sammuel <danielsamm62@gmail.com>
2022-02-23 00:57:22 -0600

    Comunicación entre componentes - Props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import React, { Fragment } from 'react';
 //FC
 
-const saludo = 'Hola Mundo';
+const PrimeraApp = ({saludo = 'Hola Mundo'}) => {
     return (
         <>
             <h1>{saludo}</h1>
